d7a113d172e40026d5332f6c06105ede0d20953a
Tarun Prahladka <tarunprahladka965@gmail.com>
2020-07-12 23:21:43 +0530

    hii

---
+++ add.c
@@ -0,0 +1,12 @@
+  #include<stdio.h>
+   
+   int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("enter the value of a:");
+     scanf("%d",&a);
+    printf("enter the value of b:");
+    scanf("%d",&b);
+    printf("the sum is %d\n",a+b);
+     return 0;
+}
